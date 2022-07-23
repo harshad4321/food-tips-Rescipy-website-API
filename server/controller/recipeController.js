ae1a0bed5ac3ec598f3adf7ae83e0dc0da0cdf18
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-23 12:37:31 +0530

    searchRecipe is setting

--- server/controller/recipeController.js
+++ server/controller/recipeController.js
@@ -66,9 +66,9 @@ exports.exploreRecipe = async(req, res) => {
 exports.exploreCategoriesById = async(req, res) => { 
   try {
     let categoryId = req.params.id;
-    const limitNumber = 20;
-    const categoryById = await Recipe.find({ 'category': categoryId }).limit(limitNumber);
-    res.render('categories', {  categoryById } );
+    const limitNumber = 7;
+    const categoryById = await Recipe.find({ 'categories': categoryId }).limit(limitNumber);
+    res.render('categories', {categoryById} );
   } catch (error) {
     res.satus(500).send({message: error.message || "Error Occured" });
   }
--- server/routes/index.js
+++ server/routes/index.js
@@ -12,7 +12,7 @@ router.get('/', recipeController.homepage )
 router.get('/recipe/:id', recipeController.exploreRecipe );
 router.get('/categories', recipeController.exploreCategories);
 router.get('/categories/:id', recipeController.exploreCategoriesById);
-
+router.post('/search', recipeController.searchRecipe);
 module.exports = router;
 
 
