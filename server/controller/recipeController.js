290e6b9f72f3ffb49185c5f80042092fa50f8a20
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-23 23:15:55 +0530

    explore-random & explore-latest set

--- server/controller/recipeController.js
+++ server/controller/recipeController.js
@@ -102,3 +102,18 @@ exports.exploreLatest = async(req, res) => {
   }
 } 
 
+
+/**
+ * GET /explore-random
+ * Explore Random as JSON
+*/
+exports.exploreRandom = async(req, res) => {
+  try {
+    let count = await Recipe.find().countDocuments();
+    let random = Math.floor(Math.random() * count);
+    let recipe = await Recipe.findOne().skip(random).exec();
+    res.render('explore-random', { title: 'Cooking Blog - Explore Latest', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
--- server/routes/index.js
+++ server/routes/index.js
@@ -14,6 +14,7 @@ router.get('/categories', recipeController.exploreCategories);
 router.get('/categories/:id', recipeController.exploreCategoriesById);
 router.post('/search', recipeController.searchRecipe );
 router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
 module.exports = router;
 
 
