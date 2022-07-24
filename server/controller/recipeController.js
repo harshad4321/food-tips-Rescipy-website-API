cda40eab98de04de06d36c6332a13b735afcf176
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-24 21:20:29 +0530

    submit form  is set

--- server/controller/recipeController.js
+++ server/controller/recipeController.js
@@ -128,3 +128,48 @@ exports.submitRecipe = async(req, res) => {
 }
 
 
+/**
+ * POST /submit-recipe
+ * Submit Recipe
+*/
+exports.submitRecipeOnPost = async(req, res) => {
+  try {
+
+    let imageUploadFile;
+    let uploadPath;
+    let newImageName;
+
+    if(!req.files || Object.keys(req.files).length === 0){
+      console.log('No Files where uploaded.');
+    } else {
+
+      imageUploadFile = req.files.image;
+      newImageName = Date.now() + imageUploadFile.name;
+
+      uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+
+      imageUploadFile.mv(uploadPath, function(err){
+        if(err) return res.satus(500).send(err);
+      })
+
+    }
+
+    const newRecipe = new Recipe({
+      name: req.body.name,
+      description: req.body.description,
+      email: req.body.email,
+      ingredients: req.body.ingredients,
+      category: req.body.category,
+      image: newImageName
+    });
+    
+    await newRecipe.save();
+
+    // req.flash('infoSubmit', 'Recipe has been added.')
+    res.redirect('/submit-recipe');
+  } catch (error) {
+    // res.json(error);
+    // req.flash('infoErrors', error);
+    res.redirect('/submit-recipe');
+  }
+}
