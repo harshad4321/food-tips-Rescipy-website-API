68f9ca6fe97a7dcbd9e06549bb34557aa30672da
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-25 11:29:35 +0530

    .

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 var logger = require('morgan');
 const dotenv= require('dotenv');
 var indexRouter = require('./server/routes/index');
@@ -19,7 +20,7 @@ app.set('view engine', 'hbs');
 
 app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}));
 
-
+app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- server/controller/recipeController.js
+++ server/controller/recipeController.js
@@ -123,8 +123,9 @@ exports.exploreRandom = async(req, res) => {
  * Submit Recipe
 */
 exports.submitRecipe = async(req, res) => {
- 
-  res.render('submit-recipe', { title: 'food&tips - Submit Recipe'} );
+  const infoErrorsObj = req.flash('infoErrors');
+  const infoSubmitObj = req.flash('infoSubmit');
+  res.render('submit-recipe', { title: 'food&tips - Submit Recipe', infoErrorsObj, infoSubmitObj} );
 }
 
 
@@ -142,12 +143,9 @@ exports.submitRecipeOnPost = async(req, res) => {
     if(!req.files || Object.keys(req.files).length === 0){
       console.log('No Files where uploaded.');
     } else {
-
       imageUploadFile = req.files.image;
       newImageName = Date.now() + imageUploadFile.name;
-
       uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
-
       imageUploadFile.mv(uploadPath, function(err){
         if(err) return res.satus(500).send(err);
       })
@@ -165,11 +163,11 @@ exports.submitRecipeOnPost = async(req, res) => {
     
     await newRecipe.save();
 
-    // req.flash('infoSubmit', 'Recipe has been added.')
+     req.flash('infoSubmit', 'Recipe has been added.')
     res.redirect('/submit-recipe');
   } catch (error) {
     // res.json(error);
-    // req.flash('infoErrors', error);
+     req.flash('infoErrors', error);
     res.redirect('/submit-recipe');
   }
 }
